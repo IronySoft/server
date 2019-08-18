9118a47250ea04577701af4bc064d89428f4a538
IronySoft <ironysoft.ltd@gmail.com>
2019-08-18 16:44:10 +0600

    brand resource added

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Brand;
 use Illuminate\Http\Request;
+use \App\Http\Resources\Brand as BrandResource;
 
 class BrandController extends Controller
 {
 
     public function index()
     {
-        return response()->json([
-            'brands' => Brand::all()
-        ], 200);
+//        return response()->json([
+//            'brands' => Brand::all()
+//        ], 200);
+
+        return BrandResource::collection(Brand::paginate(5));
     }
 
     /**
